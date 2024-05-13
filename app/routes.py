c5ece9e42dcebb2c6f80d06247be7c98b09d1403
Ydabab <krysti248@gmail.com>
2024-05-13 14:48:26 +0200

    2024-05-13 KB

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
+    if request.method=="POST":
+        product_id = request.form.get('product_id')
+        return redirect(url_for('product', product_id=product_id))
     return render_template("extract.html")
 
 @app.route('/products')
@@ -21,7 +24,3 @@ def about():
 @app.route('/product/<product_id>')
 def product(product_id):
     return render_template("product.html", product_id=product_id)
-
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
