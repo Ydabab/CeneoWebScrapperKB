1619e49d9d766a9ff394cfcc4bb88c3a5eac277a
Ydabab <krysti248@gmail.com>
2024-06-10 10:43:40 +0200

    2024-06-03 KB

--- app/routes.py
+++ app/routes.py
@@ -69,11 +69,11 @@ def extract():
 @app.route('/products')
 def products():
     products_list = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
-    products = []
-    for product_id in products_list:
-        with open(f"app/products/{product_id}.json", "r", encoding="UTF-8") as jf:
-           products.append(json.load(jf)) 
-    return render_template("products.html", products = products)
+    products=[]
+    for product_id  in products_list:
+        with open(f"app/products/{product_id}.json","r",encoding="UTF-8") as jf:
+            products.append(json.load(jf))
+    return render_template("products.html",products=products)
 
 @app.route('/about')
 def about():
@@ -81,7 +81,19 @@ def about():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
+    product_file_path = os.path.join('app/products', f'{product_id}.json')
+    product_data = None
+    try:
+        with open(product_file_path, 'r', encoding='utf-8') as file:
+            product_data = json.load(file)
+    except FileNotFoundError:
+        f"Product with ID {product_id} not found"
+    except json.JSONDecodeError:
+        f"Error decoding JSON for product with ID {product_id}"
+    if product_data is None:
+        f"Unexpected error for product with ID {product_id}"
+
+    return render_template("product.html", product=product_data,product_id=product_id)
 
 @app.route('/product/download_json/<product_id>')
 def download_json(product_id):
